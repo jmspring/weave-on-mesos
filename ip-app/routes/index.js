13faa6a678a103fb62d7cfd3b071f1f412fd3466
James Spring <jims@boinger.T-mobile.com>
2016-02-01 09:11:09 -0800

    make output more human readable

---
+++ ip-app/routes/index.js
@@ -0,0 +1,37 @@
+const os = require('os');
+const dns = require('dns');
+
+// Get the home page.
+exports.index = function(req, res){
+    var hostinfo = {};
+    
+    // Get the hostname
+    hostinfo['hostname'] = os.hostname();
+    
+    // Get the IPv4 addresses for each network interface
+    hostinfo['interfaces'] = {};
+    var interfaces = os.networkInterfaces();
+    for(var i in interfaces) {
+        var iaddr = []
+        for(var a = 0; a < interfaces[i].length; a++) {
+            if(interfaces[i][a].family == 'IPv4') {
+                iaddr.push(interfaces[i][a].address)
+            }
+        }
+        if(iaddr.length > 0) {
+            hostinfo['interfaces'][i] = iaddr;
+        }
+    }
+    
+    // Look up hostname addresses
+    dns.resolve4(hostinfo['hostname'], (err, addresses) => {
+        if(err) throw err;
+        
+        hostinfo['hostname_ips'] = addresses;
+        
+        console.log(hostinfo);
+        
+        // Send response
+        res.render('index', { hostinfo: hostinfo });
+    });
+};
--- ip-app/server.js
+++ ip-app/server.js
@@ -1,12 +1,19 @@
 'use strict';
 
 const express = require('express');
-const os = require('os');
-const dns = require('dns');
+const path = require('path');
+
+const routes = require('./routes');
 
 const PORT = 8080;
 
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*
 app.get('/', function (req, res) {
     var result = {};
     
@@ -38,6 +45,9 @@ app.get('/', function (req, res) {
         res.send(JSON.stringify(result, null, 2));
     });
 });
+*/
+
+app.get('/', routes.index);
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
